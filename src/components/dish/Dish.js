0bb1a5c883bc2d13f8f1dd7c2fbe92e223a45f6d
zgs3 <zkairaitis@gmail.com>
2022-07-15 14:24:02 +0300

    Created Dishes CRUD page

---
+++ src/components/dish/Dish.js
@@ -0,0 +1,298 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Dish.module.css'
+
+function Dish() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const [dishes, setDishes] = useState([]);
+  const [dish, setDish] = useState([]);
+
+  // ALL DISHES
+  const [newDish, setNewDish] = useState({});
+  const [dishName, setDishName] = useState('');
+  const [dishPrice, setDishPrice] = useState('');
+  const [imageLink, setImageLink] = useState('');
+  const [restaurantId, setRestaurantId] = useState('');
+
+  // DISH TO EDIT
+  const [newName, setNewName] = useState('');
+  const [newPrice, setNewPrice] = useState('');
+  const [newImageLink, setNewImageLink] = useState('');
+  const [newRestaurantId, setNewRestaurantId] = useState('');
+  const [editedDish, setEditedDish] = useState({});
+  
+  const [showDiv, setShowDiv] = useState(false);
+  
+  // RATINGS
+  const [ratings, setRatings] = useState([]);
+
+
+  function deleteDish(id, e) {
+    fetch("http://127.0.0.1:8000/api/v1/dishes/" + id, { method: 'DELETE' })
+      .then((response) => {
+        if (response.status === 200) {
+          const remaining = dishes.filter(item => id !== item.id)
+          setDishes(remaining)
+        }
+      });
+  }
+
+  function addDish(e) {
+    e.preventDefault();
+    fetch("http://127.0.0.1:8000/api/v1/dishes", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newDish)
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          setDishName('');
+          setDishPrice('');
+          setImageLink('');
+          setRestaurantId('');
+          fetch("http://127.0.0.1:8000/api/v1/dishes")
+            .then(res => res.json())
+            .then(
+              (result) => {
+                setDishes(result);
+              })
+        }
+      })
+  }
+
+  function showEdit(id) {
+    setShowDiv(!showDiv);
+    fetch("http://127.0.0.1:8000/api/v1/dishes/" + id)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDish(result);
+        })
+  }
+  
+  function updateDish(id, e) {
+    e.preventDefault();
+    fetch("http://127.0.0.1:8000/api/v1/dishes/" + id, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(editedDish)
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          fetch("http://127.0.0.1:8000/api/v1/dishes")
+            .then(res => res.json())
+            .then(
+              (result) => {
+                setDishes(result);
+                setShowDiv(!showDiv);
+              })
+        }
+      })
+  }
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/v1/dishes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDishes(result);
+          setIsLoaded(true);
+        },
+        (error) => { setError(error); setIsLoaded(true); })
+  }, [])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/v1/ratings")
+      .then(res => res.json())
+      .then(result => setRatings(result));
+  }, [])
+
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>Error: {error.message}</div>;
+  } else {
+    return (
+      <>
+        <div className="container">
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Photo</th>
+                <th>Rating</th>
+                <th>Available in</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dishes.map(dish => (
+                <tr key={dish.id}>
+                  <td>{dish.name}</td>
+                  <td>{dish.price} Eur.</td>
+                  <td> <img src={dish.image_link} alt='Photo of the dish'></img> </td>
+                  <td> TBA </td>
+                  <td>{dish.restaurant.title}</td>
+
+                  <td>
+                    <button
+                      className="btn btn-dark"
+                      onClick={(e) => deleteDish(dish.id, e)}>
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-primary"
+                      onClick={(e) => {
+                        showEdit(dish.id)
+                      }}>
+                      Edit
+                    </button>
+                  </td>
+                </tr>)
+              )}
+            </tbody>
+          </table>
+        </div >
+
+        <div className='container'>
+          {(showDiv)
+            ?
+            <div className="container">
+              <div className="card p-5">
+                <h2 className='mb-3'>Update: {dish.name}</h2>
+                <form className='row g-3'>
+                  <div className='row-md-6'>
+                    <input
+                      type="text"
+                      name='name'
+                      className='form-control m-auto'
+                      placeholder={dish.name}
+                      onChange={(e) => {
+                        setNewName(e.target.value);
+                        setEditedDish({ ...editedDish, name: e.target.value });
+                      }} />
+                  </div>
+
+                  <div className='row-md-6'>
+                    <input
+                      type="number"
+                      name='price'
+                      placeholder={dish.price}
+                      className='form-control m-auto'
+                      onChange={(e) => {
+                        setNewPrice(e.target.value);
+                        setEditedDish({ ...editedDish, price: e.target.value });
+                      }} />
+                  </div>
+
+                  <div className='row-md-6'>
+                    <input
+                      type="text"
+                      name='imageLink'
+                      placeholder={dish.image_link}
+                      className='form-control m-auto'
+                      onChange={(e) => {
+                        setNewImageLink(e.target.value);
+                        setEditedDish({ ...editedDish, image_link: e.target.value });
+                      }} />
+                  </div>
+                  <div className='row-md-6'>
+                    <input
+                      type="number"
+                      name='restaurantId'
+                      placeholder={dish.restaurant_id}
+                      className='form-control m-auto'
+                      onChange={(e) => {
+                        setNewRestaurantId(e.target.value);
+                        setEditedDish({ ...editedDish, restaurant_id: e.target.value });
+                      }} />
+                  </div>
+
+                  <div className='my-4'>
+                    <input className='btn btn-success px-5' type='submit' value='Edit' onClick={(e) => updateDish(dish.id, e)} />
+                  </div>
+                </form>
+
+              </div>
+            </div>
+            :
+            <div>
+
+              <div className="container">
+                <div className="card p-5">
+                  <h2 className='mb-3'>Add new dish</h2>
+                  <form className='row g-3'>
+                    <div className='row-md-6'>
+                      <input
+                        type="text"
+                        name='title'
+                        className='form-control m-auto'
+                        placeholder='Name'
+                        value={dishName}
+                        onChange={(e) => {
+                          setDishName(e.target.value);
+                          setNewDish({ ...newDish, name: e.target.value });
+                        }} />
+                    </div>
+                    <div className='row-md-6'>
+                      <input
+                        type="number"
+                        name='price'
+                        placeholder='Price in Euros'
+                        className='form-control m-auto'
+                        value={dishPrice}
+                        onChange={(e) => {
+                          setDishPrice(e.target.value);
+                          setNewDish({ ...newDish, price: e.target.value });
+                        }} />
+                    </div>
+                    <div className='row-md-6'>
+                      <input
+                        type="text"
+                        name='imageLink'
+                        placeholder='Image link'
+                        className='form-control m-auto'
+                        value={imageLink}
+                        onChange={(e) => {
+                          setImageLink(e.target.value);
+                          setNewDish({ ...newDish, image_link: e.target.value });
+                        }} />
+                    </div>
+                    <div className='row-md-6'>
+                      <input
+                        type="number"
+                        name='restaurant'
+                        placeholder='Restaurant'
+                        className='form-control m-auto'
+                        value={restaurantId}
+                        onChange={(e) => {
+                          setRestaurantId(e.target.value);
+                          setNewDish({ ...newDish, restaurant_id: e.target.value });
+                        }} />
+                    </div>
+
+                    <div className='my-4'>
+                      <input className='btn btn-success px-5' type='submit' value='Add' onClick={(e) => addDish(e)} />
+                    </div>
+
+                  </form>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dish;
